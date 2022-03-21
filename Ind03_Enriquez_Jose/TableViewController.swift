fe746ff1f32751ae76c969d003bcfd4ee7049a9c
Jose Enriquez <joseenriquez9891@gmail.com>
2022-03-21 14:16:53 -0500

    sends the correct state object to the second viewController. All it needs are minor UI adjustments and replace the placeholder images with the real images.

--- Ind03_Enriquez_Jose/TableViewController.swift
+++ Ind03_Enriquez_Jose/TableViewController.swift
@@ -10,114 +10,59 @@ import UIKit
 class TableViewController: UITableViewController {
     let states: [State] = [
         State(name: "Alabama", nickName: "Yellowhammer State", area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
-//        State(name: "Alaska", nickName: "The Last Frontier", area: "", flag: UIImage(named: "poweroff"), map: UIImage(named: "poweroff")),
-//        State(name: "Arizona", nickName: "The Grand Canyon State", area: "", flag: UIImage(named: "poweroff"), map: UIImage(named: "poweroff")),
-//        State(name: "Arkansas", nickName: "The Natural State",area: "", flag: UIImage(named: "poweroff"), map: UIImage(named: "poweroff")),
-//        State(name: "California", nickName: "The Golden State", area: "", flag: UIImage(named: "poweroff"), map: UIImage(named: "poweroff"))
-//        State(name: "Colorado", nickName: "The Centennial State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Connecticut", nickName: "The Constitution State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Delaware", nickName: "The First State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Florida", nickName: "The Sunshine State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Georgia", nickName: "The Peach State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Hawaii", nickName: "The Aloha State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Idaho", nickName: "The Gem State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Illinois", nickName: "Prairie State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Indiana", nickName: "The Hoosier State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Iowa", nickName: "The Hawkeye State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Kansas", nickName: "The Sunflower State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Kentucky", nickName: "The Bluegrass State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Louisiana", nickName: "The Pelican State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Maine", nickName: "The Pine Tree State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Maryland", nickName: "The Old Line State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Massachusetts", nickName: "The Bay State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Michigan", nickName: "The Greak Lakes State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Minnesota", nickName: "The North Star State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Mississippi", nickName: "The Magnolia State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Missouri", nickName: "The Show Me State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Montana", nickName: "The Treasure State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Nebraska", nickName: "The Cornhusker State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Nevada", nickName: "The Silver State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "New Hampshire", nickName: "The Granite State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "New Jersey", nickName: "The Garden State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "New Mexico", nickName: "The Land of Enchantment State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "New York", nickName: "The Empire State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "North Carolina", nickName: "The Tar Heel State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "North Dakota", nickName: "The Peace Garden State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Ohio", nickName: "The Buckeye State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Oklahoma", nickName: "The Sooner State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Oregon", nickName: "The Beaver State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Pennsylvania", nickName: "The Keystone State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Rhode Island", nickName: "The Ocean State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "South Carolina", nickName: "The Palmetto State", area: "", flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "South Dakota", nickName: "Mount Rushmore State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Tennesse", nickName: "The Volunteer State", area: "", flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Texas", nickName: "The Lone Star State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Utah", nickName: "The Behive State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Vermont", nickName: "The Green Mountain State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Virginia", nickName: "The Old Dominion State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Washington", nickName: "The Evergreen State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "West Virginia", nickName: "The Mountain State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Wisconsin", nickName: "The Badger State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>),
-//        State(name: "Wyoming", nickName: "The Equality or Cowboy State", area: <#T##String#>, flag: <#T##UIImage#>, map: <#T##UIImage#>)
+        State(name: "Alaska", nickName: "The Last Frontier", area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Arizona", nickName: "The Grand Canyon State", area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Arkansas", nickName: "The Natural State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "California", nickName: "The Golden State", area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Colorado", nickName: "The Centennial State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Connecticut", nickName: "The Constitution State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Delaware", nickName: "The First State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Florida", nickName: "The Sunshine State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Georgia", nickName: "The Peach State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Hawaii", nickName: "The Aloha State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Idaho", nickName: "The Gem State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Illinois", nickName: "Prairie State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Indiana", nickName: "The Hoosier State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Iowa", nickName: "The Hawkeye State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Kansas", nickName: "The Sunflower State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Kentucky", nickName: "The Bluegrass State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Louisiana", nickName: "The Pelican State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Maine", nickName: "The Pine Tree State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Maryland", nickName: "The Old Line State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Massachusetts", nickName: "The Bay State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Michigan", nickName: "The Greak Lakes State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Minnesota", nickName: "The North Star State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Mississippi", nickName: "The Magnolia State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Missouri", nickName: "The Show Me State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Montana", nickName: "The Treasure State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Nebraska", nickName: "The Cornhusker State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Nevada", nickName: "The Silver State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "New Hampshire", nickName: "The Granite State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "New Jersey", nickName: "The Garden State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "New Mexico", nickName: "The Land of Enchantment State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "New York", nickName: "The Empire State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "North Carolina", nickName: "The Tar Heel State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "North Dakota", nickName: "The Peace Garden State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Ohio", nickName: "The Buckeye State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Oklahoma", nickName: "The Sooner State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Oregon", nickName: "The Beaver State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Pennsylvania", nickName: "The Keystone State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Rhode Island", nickName: "The Ocean State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "South Carolina", nickName: "The Palmetto State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "South Dakota", nickName: "Mount Rushmore State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Tennesse", nickName: "The Volunteer State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Texas", nickName: "The Lone Star State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Utah", nickName: "The Behive State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Vermont", nickName: "The Green Mountain State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Virginia", nickName: "The Old Dominion State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Washington", nickName: "The Evergreen State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "West Virginia", nickName: "The Mountain State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Wisconsin", nickName: "The Badger State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill")),
+        State(name: "Wyoming", nickName: "The Equality or Cowboy State",area: "", flag: UIImage(systemName: "heart.fill"), map: UIImage(systemName: "heart.fill"))
     ]
     
     
     
-    let stateNames: [(String, String)] = [
-        
-        (name: "Alabama", nickName: "Yellowhammer State"),
-        (name: "Alaska", nickName: "The Last Frontier"),
-        (name: "Arizona", nickName: "The Grand Canyon State"),
-        (name: "Arkansas", nickName: "The Natural State"),
-        (name: "California", nickName: "The Golden State"),
-        (name: "Colorado", nickName: "The Centennial State"),
-        (name: "Connecticut", nickName: "The Constitution State"),
-        (name: "Delaware", nickName: "The First State"),
-        (name: "Florida", nickName: "The Sunshine State"),
-        (name: "Georgia", nickName: "The Peach State"),
-        (name: "Hawaii", nickName: "The Aloha State"),
-        (name: "Idaho", nickName: "The Gem State State"),
-        (name: "Illinois", nickName: "Prairie State"),
-        (name: "Indiana", nickName: "The Hoosier State"),
-        (name: "Iowa", nickName: "The Hawkeye State"),
-        (name: "Kansas", nickName: "The Sunflower State"),
-        (name: "Kentucky", nickName: "The Bluegrass State"),
-        (name: "Louisiana", nickName: "The Pelican State"),
-        (name: "Maine", nickName: "The Pine Tree State"),
-        (name: "Maryland", nickName: "The Old Line State"),
-        (name: "Massachusetts", nickName: "The Bay State"),
-        (name: "Michigan", nickName: "The Greak Lakes State"),
-        (name: "Minnesota", nickName: "The North Star State"),
-        (name: "Mississippi", nickName: "The Magnolia State"),
-        (name: "Missouri", nickName: "The Show Me State"),
-        (name: "Montana", nickName: "The Treasure State"),
-        (name: "Nebraska", nickName: "The Cornhusker State"),
-        (name: "Nevada", nickName: "The Silver State"),
-        (name: "New Hampshire", nickName: "The Granite State"),
-        (name: "New Jersey", nickName: "The Garden State"),
-        (name: "New Mexico", nickName: "The Land of Enchantment State"),
-        (name: "New York", nickName: "The Empire State"),
-        (name: "North Carolina", nickName: "The Tar Heel State"),
-        (name: "North Dakota", nickName: "The Peace Garden State"),
-        (name: "Ohio", nickName: "The Buckeye State"),
-        (name: "Oklahoma", nickName: "The Sooner State"),
-        (name: "Oregon", nickName: "The Beaver State"),
-        (name: "Pennsylvania", nickName: "The Keystone State"),
-        (name: "Rhode Island", nickName: "The Ocean State"),
-        (name: "South Carolina", nickName: "The Palmetto State"),
-        (name: "South Dakota", nickName: "Mount Rushmore State"),
-        (name: "Tennesse", nickName: "The Volunteer State"),
-        (name: "Texas", nickName: "The Lone Star State"),
-        (name: "Utah", nickName: "The Behive State"),
-        (name: "Vermont", nickName: "The Green Mountain State"),
-        (name: "Virginia", nickName: "The Old Dominion State"),
-        (name: "Washington", nickName: "The Evergreen State"),
-        (name: "West Virginia", nickName: "The Mountain State"),
-        (name: "Wisconsin", nickName: "The Badger State"),
-        (name: "Wyoming", nickName: "The Equality or Cowboy State")
-       
-        
-    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -140,7 +85,7 @@ class TableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return stateNames.count
+        return states.count
     }
 
     
@@ -149,12 +94,11 @@ class TableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
         // Configure the cell...
-        let cellData: (name: String, nickName: String) = stateNames[indexPath[1]]
+        let cellData: (name: String, nickName: String) = (states[indexPath[1]].name, states[indexPath[1]].nickName)
         
         cell.textLabel?.text = cellData.name
 
         return cell
-        
     }
     
 
@@ -204,9 +148,12 @@ class TableViewController: UITableViewController {
     */
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let secondScreen = segue.destination as! ViewController
-        let cell = sender as? UITableViewCell
         
-        secondScreen.test = (cell?.textLabel?.text)!
+        let indexPath = self.tableView.indexPathForSelectedRow!
+        let cellState = states[indexPath.row]
+        secondScreen.state = cellState
+        
+        self.tableView.deselectRow(at: indexPath, animated: true)
     }
 
 }
